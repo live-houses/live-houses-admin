c3ba9d5e7af1e549d9fef43ff3d6e8c6dbe07abd
luedu1103 <luis.calle3@unmsm.edu.pe>
2023-10-16 00:57:43 -0500

    E3: More values

--- design/schema/SomeValues.sql
+++ design/schema/SomeValues.sql
@@ -2,6 +2,12 @@ SELECT * FROM account;
 
 --This adds some values to the tables of roles and accounts
 INSERT INTO role (id, role_name, permissions)
-VALUES (3, 'admin1', 00101010);
+VALUES (1, 'admin1', 11);
+INSERT INTO role (id, role_name, permissions)
+VALUES (2, 'admin2', 10);
+INSERT INTO role (id, role_name, permissions)
+VALUES (3, 'normal_user', 00);
 INSERT INTO account (id, role_id, username, email, hashed_password, salt) 
-VALUES (2, 3,'admin1', 'unmsm', 123, 45);
+VALUES (1, 2,'Paolo Flores', 'unmsm', 123, 45);
+INSERT INTO account (id, role_id, username, email, hashed_password, salt) 
+VALUES (2, 2,'Rodrigo Alva', 'unmsm', 123, 45);
