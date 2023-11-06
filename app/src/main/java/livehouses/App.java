5b818cf4a8102fc2f508ef3f09b07693a895e7f9
Paolo Flores <pflores.fisi22@gmail.com>
2023-11-06 03:39:04 -0500

    fix: make login resizable

--- app/src/main/java/livehouses/App.java
+++ app/src/main/java/livehouses/App.java
@@ -18,7 +18,6 @@ public class App extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
-        stage.setResizable(false);
         stage.centerOnScreen();
         stage.show();
     }
